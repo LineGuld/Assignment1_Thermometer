5150fde5c34779a59b02ff48113be92541f5cc18
Firrez <246299@via.dk>
2021-02-22 14:01:01 +0100

    Removed redundant methods

--- src/util/PropertyChangeSubject.java
+++ src/util/PropertyChangeSubject.java
@@ -4,9 +4,6 @@ import java.beans.PropertyChangeListener;
 
 public interface PropertyChangeSubject
 {
-  void addPropertyChangeListener(String name, PropertyChangeListener listener);
   void addPropertyChangeListener(PropertyChangeListener listener);
-  void removePropertyChangeListener(String name,
-      PropertyChangeListener listener);
   void removePropertyChangeListener(PropertyChangeListener listener);
 }
